28bfeb2740ea1e6cf14fe785b6e87c0059128a23
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2022-10-28 15:20:04 +0500

    data from redux store shown in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import { useSelector } from "react-redux";
 
 const Header = () => {
-  //   const result = useSelector((state) => state.cartData);
-  //   console.warn("data in header", result);
+  const result = useSelector((state) => state.cartData);
+
+  console.log("data in header", result);
   return (
     <div className="header">
       <div className="cart-div">
-        <span>{/* {result.length} */}1</span>
+        <span>{result.length}</span>
         <img
           src="https://cdn-icons-png.flaticon.com/512/263/263142.png"
           alt=""
