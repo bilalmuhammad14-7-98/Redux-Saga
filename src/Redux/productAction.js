4615a78ce927a9a1a4bfa02c0f7816b6f782899e
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2022-10-28 20:08:57 +0500

    calling api with redux saga

--- src/Redux/productAction.js
+++ src/Redux/productAction.js
@@ -2,9 +2,8 @@ import { PRODUCT_LIST } from "./constants";
 
 export const productList = () => {
   console.log("product list action is called");
-  let data = "hello";
+
   return {
     type: PRODUCT_LIST,
-    data,
   };
 };
--- src/Redux/productReducer.js
+++ src/Redux/productReducer.js
@@ -1,13 +1,12 @@
-import { PRODUCT_LIST } from "./constants";
+import { SET_PRODUCT_LIST } from "./constants";
 
 export const productData = (data = [], action) => {
   console.log(action, "reducer called");
 
   switch (action.type) {
-    case PRODUCT_LIST:
-      console.log("PRODUCT_LIST condition", action);
-      return [action.data];
-
+    case SET_PRODUCT_LIST:
+      console.log("SET_PRODUCT_LIST condition", action);
+      return [...action.data];
     default:
       return data;
   }
